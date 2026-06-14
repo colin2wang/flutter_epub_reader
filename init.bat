bd3bf442b2a13c818d737c56944f5b0cde225325
WangBing <colin2wang@163.com>
2026-06-14 13:46:18 +0800

    chore(init): simplify script and remove flutter create

--- init.bat
+++ init.bat
@@ -1,23 +1,20 @@
 @echo off
-setlocal
 
 echo === Flutter EPUB Reader - Project Init ===
 
-echo [1/4] Regenerating Android build files...
-flutter create --org com.colin2wang . >nul 2>&1
+echo [1/3] Removing stale v1 GeneratedPluginRegistrant...
+if not exist "android\app\src\main\java\io\flutter\plugins" goto :skip_rmdir
+rmdir /s /q "android\app\src\main\java\io\flutter\plugins"
+echo       Done.
+goto :after_rmdir
+:skip_rmdir
+echo       Skipped (not found).
+:after_rmdir
 
-echo [2/4] Removing stale v1 GeneratedPluginRegistrant...
-if exist "android\app\src\main\java\io\flutter\plugins" (
-    rmdir /s /q "android\app\src\main\java\io\flutter\plugins"
-    echo       Done.
-) else (
-    echo       Skipped (not found).
-)
-
-echo [3/4] Configuring Gradle mirror (Tencent)...
+echo [2/3] Configuring Gradle mirror (Tencent)...
 powershell -Command "(Get-Content 'android\gradle\wrapper\gradle-wrapper.properties') -replace 'https\\://services.gradle.org/distributions/', 'https\\://mirrors.cloud.tencent.com/gradle/' | Set-Content 'android\gradle\wrapper\gradle-wrapper.properties'"
 
-echo [4/4] Getting dependencies...
+echo [3/3] Getting dependencies...
 flutter pub get
 
-echo === Init complete. Run 'flutter build apk' to build. ===
+echo === Init complete. Run 'flutter run' to start. ===
